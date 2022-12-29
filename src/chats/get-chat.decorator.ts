640804c04bf2bdf01a430c3ad0628410f88f5b3c
nicolaslazzos <nicolaslazzos09@gmail.com>
2022-12-28 22:45:21 -0300

    deleting unused decorator

--- src/chats/get-chat.decorator.ts
+++
@@ -1,3 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-export const GetChat = createParamDecorator((_, ctx: ExecutionContext): string => ctx.switchToHttp().getRequest().headers?.chat);
